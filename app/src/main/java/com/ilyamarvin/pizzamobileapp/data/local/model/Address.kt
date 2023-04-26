2024d2c72d1d3e52d0aca40d4d3167e0be8ecda9
IlyaMarvin <ilya.sereda2002@mail.ru>
2023-04-26 18:49:28 +0300

    Added data models for addresses

--- app/src/main/java/com/ilyamarvin/pizzamobileapp/data/local/model/Address.kt
+++ app/src/main/java/com/ilyamarvin/pizzamobileapp/data/local/model/Address.kt
@@ -1,16 +1,18 @@
-package com.ilyamarvin.pizzamobileapp.data.local.models
+package com.ilyamarvin.pizzamobileapp.data.local.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "addresses")
+@Entity(tableName = "address")
 data class Address(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
+    val uid: Int,
     var street: String,
     var apartment: Int,
     var floor: Int,
     var entrance: Int,
     var intercom: Int,
-    var comment: String
+    var comment: String,
+    var isDefault: Int
 )
